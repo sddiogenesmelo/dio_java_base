93e5f42d568d5f7d0240abd9d4c7c65e1f72c5be
Diogenes Melo <103974320+sddiogenesmelo@users.noreply.github.com>
2024-08-01 21:45:25 -0300

    modified

--- tipos_variaveis/src/tipos_variaveis.java
+++ tipos_variaveis/src/tipos_variaveis.java
@@ -1,11 +1,13 @@
 public class tipos_variaveis {
     public static void main(String[] args) throws Exception {
-                    
-        byte idade = 123;
-        short ano = 2021;
-        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
-        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
-        float pi = 3.14F;
-        double salario = 1275.33;
+    
+        double salarioMinimo = 2500.33;
+
+        int numero = 5;
+        
+        numero = 10;
+
+        System.out.print(numero);
+
     }
 }
